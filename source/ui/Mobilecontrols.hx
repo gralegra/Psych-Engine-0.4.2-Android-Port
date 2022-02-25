447d7bca4ac72e38cda96287af9daccb69e6f7ce
gralegra <100141683+gralegra@users.noreply.github.com>
2022-02-25 10:54:38 -0300

    Update Mobilecontrols.hx

--- source/ui/Mobilecontrols.hx
+++ source/ui/Mobilecontrols.hx
@@ -1,14 +1,72 @@
-package ui;
+package android;
 
 import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
+import flixel.util.FlxSave;
+import flixel.math.FlxPoint;
 
-import ui.FlxVirtualPad;
-import ui.Hitbox;
+import android.FlxVirtualPad;
+import android.Hitbox;
 
-import Config;
+class Config {
+	var save:FlxSave;
 
-class Mobilecontrols extends FlxSpriteGroup
+	public function new() {
+		save = new FlxSave();
+		save.bind("saveconrtol");
+	}
+
+	public function getcontrolmode():Int {
+		if (save.data.buttonsmode != null) 
+			return save.data.buttonsmode[0];
+		return 0;
+	}
+
+	public function setcontrolmode(mode:Int = 0):Int {
+		if (save.data.buttonsmode == null) save.data.buttonsmode = new Array();
+		save.data.buttonsmode[0] = mode;
+		save.flush();
+
+		return save.data.buttonsmode[0];
+	}
+
+	public function savecustom(_pad:FlxVirtualPad) {
+		if (save.data.buttons == null)
+		{
+			save.data.buttons = new Array();
+
+			for (buttons in _pad)
+			{
+				save.data.buttons.push(FlxPoint.get(buttons.x, buttons.y));
+			}
+		}
+		else
+		{
+			var tempCount:Int = 0;
+			for (buttons in _pad)
+			{
+				save.data.buttons[tempCount] = FlxPoint.get(buttons.x, buttons.y);
+				tempCount++;
+			}
+		}
+		save.flush();
+	}
+
+	public function loadcustom(_pad:FlxVirtualPad):FlxVirtualPad {
+		if (save.data.buttons == null) 
+			return _pad;
+		var tempCount:Int = 0;
+
+		for(buttons in _pad) {
+			buttons.x = save.data.buttons[tempCount].x;
+			buttons.y = save.data.buttons[tempCount].y;
+			tempCount++;
+		}	
+		return _pad;
+	}
+}
+
+class AndroidControls extends FlxSpriteGroup
 {
 	public var mode:ControlsGroup = HITBOX;
 
@@ -23,54 +81,74 @@ class Mobilecontrols extends FlxSpriteGroup
 		
 		config = new Config();
 
-		// load control mode num from Config.hx
 		mode = getModeFromNumber(config.getcontrolmode());
-		trace(config.getcontrolmode());
 
 		switch (mode)
 		{
 			case VIRTUALPAD_RIGHT:
-				initVirtualPad(0);
+				initControler(0);
 			case VIRTUALPAD_LEFT:
-				initVirtualPad(1);
-			case VIRTUALPAD_CUSTOM:
-				initVirtualPad(2);
+				initControler(1);
+                        case VIRTUALPAD_CUSTOM:
+ 				initControler(2);                               
+			case DUO:
+				initControler(3);
 			case HITBOX:
-				_hitbox = new Hitbox();
-				add(_hitbox);
+				initControler(4);
 			case KEYBOARD:
+				// do nothing
 		}
 	}
 
-	function initVirtualPad(vpadMode:Int) 
+	function initControler(vpadMode:Int) 
 	{
 		switch (vpadMode)
 		{
+			case 0:
+				_virtualPad = new FlxVirtualPad(RIGHT_FULL, NONE);	
+				_virtualPad.alpha = 0.75;
+				add(_virtualPad);						
 			case 1:
 				_virtualPad = new FlxVirtualPad(FULL, NONE);
+				_virtualPad.alpha = 0.75;
+				add(_virtualPad);			
 			case 2:
 				_virtualPad = new FlxVirtualPad(FULL, NONE);
 				_virtualPad = config.loadcustom(_virtualPad);
-			default: // 0
-				_virtualPad = new FlxVirtualPad(RIGHT_FULL, NONE);
+				_virtualPad.alpha = 0.75;
+				add(_virtualPad);	
+			case 3:
+				_virtualPad = new FlxVirtualPad(DUO, NONE);
+				_virtualPad.alpha = 0.75;
+				add(_virtualPad);		
+			case 4:
+				_hitbox = new Hitbox();
+				add(_hitbox);		
+			default:
+				_virtualPad = new FlxVirtualPad(RIGHT_FULL, NONE);	
+				_virtualPad.alpha = 0.75;
+				add(_virtualPad);					
 		}
-		
-		_virtualPad.alpha = 0.75;
-		add(_virtualPad);	
 	}
 
 
 	public static function getModeFromNumber(modeNum:Int):ControlsGroup {
 		return switch (modeNum)
 		{
-			case 0: VIRTUALPAD_RIGHT;
-			case 1: VIRTUALPAD_LEFT;
-			case 2: KEYBOARD;
-			case 3: VIRTUALPAD_CUSTOM;
-			case 4:	HITBOX;
-
-			default: VIRTUALPAD_RIGHT;
-
+			case 0: 
+				VIRTUALPAD_RIGHT;
+			case 1: 
+				VIRTUALPAD_LEFT;
+			case 2: 
+				VIRTUALPAD_CUSTOM;
+			case 3: 
+				DUO;
+			case 4:	
+				HITBOX;
+			case 5: 
+				KEYBOARD;
+			default: 
+				VIRTUALPAD_RIGHT;
 		}
 	}
 }
@@ -78,7 +156,8 @@ class Mobilecontrols extends FlxSpriteGroup
 enum ControlsGroup {
 	VIRTUALPAD_RIGHT;
 	VIRTUALPAD_LEFT;
-	KEYBOARD;
-	VIRTUALPAD_CUSTOM;
+        VIRTUALPAD_CUSTOM;
+        DUO;
 	HITBOX;
+	KEYBOARD;
 }
